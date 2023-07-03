29cc13fe7a147651a11896afe6f89805fbd75514
luku5534 <luku5534>
2023-07-03 12:51:28 +0500

    updated auth

---
+++ controllers/AuthController.js
@@ -0,0 +1,59 @@
+const sequlize = require("../config/dbConfig");
+const {authCluster} = require("../models");
+const { authSchema, loginSchema } = require("../utils/schema");
+
+
+const authRegister = async(req, res) => {
+    try {
+        let authBodyData = await authSchema.validate(req.body);
+        console.log(authBodyData)
+        let auth = await authCluster.create(authBodyData);
+        return res.status(200).json({
+          message: "admin register successfully",
+          result: true,
+          data: auth
+        })
+    
+      } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+          message: error.message
+        })
+    
+      }
+}
+
+
+const authLogin = (req, res) => {
+  loginSchema.validate(req.body)
+    .then(async (validatedData) => {
+      let authData = await authCluster.findOne({
+        email: validatedData.email,
+      });
+
+      if (authData !== null) {
+        const passwordMatch = await comparePassword(validatedData.password, authData.password);
+        if (passwordMatch) {
+          res.status(200).json({
+            message: 'Logged in successfully!',
+            auth: authData,
+          });
+        } else {
+          res.status(403).json({
+            error: 'Invalid credentials!',
+          });
+        }
+      } else {
+        res.status(404).json({
+          error: 'An account with the given credentials not found!',
+        });
+      }
+    })
+    .catch((error) => {
+      return res.status(500).json({
+        message: error.message,
+      });
+    });
+};
+
+module.exports = { authRegister, authLogin }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-const express = require("express");
-const router = express.Router();
-const { AuthController } = require("../controllers");
-const { verifyToken } = require("../middlewares/index");
+// const express = require("express");
+// const router = express.Router();
+// const { authController } = require("../controllers");
+// const { verifyToken } = require("../middlewares/index");
 
-router.post("");
+// router.post("/register", authController.authRegister);
+// router.post("/login", authController.authLogin);
 
-module.exports = router;
+// module.exports = router;

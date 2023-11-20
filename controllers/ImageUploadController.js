736eb237ea9c2125f3fcd91143ae32b447629094
itsahmadawais <awaisahmad1276@gmail.com>
2023-11-20 18:58:55 +0500

    image upload to s3

--- controllers/ImageUploadController.js
+++ controllers/ImageUploadController.js
@@ -1,3 +1,35 @@
+const AWS = require("aws-sdk");
+const fs = require("fs");
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+});
+
+const uploadImageToS3 = async (req, res) => {
+  try {
+    const fileName =
+      Date.now().toString() + "." + req.file.mimetype.split("/")[1];
+
+    const uploadedImage = await s3
+      .upload({
+        Bucket: process.env.AWS_S3_BUCKET_NAME,
+        Key: fileName,
+        Body: req.file.buffer,
+      })
+      .promise();
+
+    return res.status(200).json({
+      imageName: fileName,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      error: "Could not upload image to s3!",
+    });
+  }
+};
+
 const uploadImage = (req, res) => {
   try {
     return res.status(200).json({
@@ -13,4 +45,5 @@ const uploadImage = (req, res) => {
 
 module.exports = {
   uploadImage,
+  uploadImageToS3,
 };
--- routes/imageUploadRoutes.js
+++ routes/imageUploadRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { ImageUploadController } = require("../controllers");
 const upload = require("../utils/multer");
 
-router.post("/", upload.single("file"), ImageUploadController.uploadImage);
+//router.post("/", upload.single("file"), ImageUploadController.uploadImage);
+router.post("/", upload.single("file"), ImageUploadController.uploadImageToS3);
 
 module.exports = router;
